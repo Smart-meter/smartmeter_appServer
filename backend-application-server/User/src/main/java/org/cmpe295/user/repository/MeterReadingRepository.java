75d81d40d2f5ec8cfce54151026f44c92fe4161c
MeeraTresa <meeratresa@gmail.com>
2024-01-11 12:14:57 -0800

    Adding application.properties and Spring Boot Applications to MeterImages module

--- backend-application-server/User/src/main/java/org/cmpe295/user/repository/MeterReadingRepository.java
+++ backend-application-server/User/src/main/java/org/cmpe295/user/repository/MeterReadingRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface MeterReadingRepository extends JpaRepository<MeterReading, Long> {
-    Optional<MeterReading> findByUtilityAccountNumber(String utilityAccountNumber);
+
 }

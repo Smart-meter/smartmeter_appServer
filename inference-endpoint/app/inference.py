da7f8a109788cadb98d090867949a60d2fea61b9
MeeraTresa <meeratresa@gmail.com>
2024-01-16 11:23:24 -0800

    Complete the inference method. Update the Dockerfile

--- inference-endpoint/app/inference.py
+++ inference-endpoint/app/inference.py
@@ -27,6 +27,24 @@ async def predict(file: UploadFile = File(...)):
     print("File Path:", file_path)
     img = cv2.imread(file_path)
     results = model(img)[0]
+    # Extracted meter reading logic
     extracted_meter_reading = 0
+    for result in results.boxes.data.tolist():
+        x1, y1, x2, y2, score, class_id = result
+        if score > threshold:
+            print("Score is more than threshold")
+            # extracting text from the cropped region
+            roi = img[int(y1):int(y2), int(x1):int(x2)]
+            temp_path = os.path.join(upload_folder, 'temp_cropped.jpg')
+            print("Saving the cropped image")
+            cv2.imwrite(temp_path, roi)
+            paddleresults = ocr.ocr(temp_path, cls=True)
+            print('Paddle Results:-->', paddleresults)
+            if paddleresults and len(paddleresults) > 0 and paddleresults[0] and len(paddleresults[0]) > 0 and \
+                    paddleresults[0][0][1] and len(paddleresults[0][0][1]) > 0:
+                extracted_meter_reading = paddleresults[0][0][1][0]
+            else:
+                extracted_meter_reading = ""
+
     # Return the extracted meter reading
     return {"meter_reading": extracted_meter_reading}

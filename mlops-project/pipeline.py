08f6796c96b09dd56d7eeb4a7cd8422a6d177794
MeeraTresa <meeratresa@gmail.com>
2024-04-25 17:45:32 -0700

    Completing the pipeline framework

--- mlops-project/pipeline.py
+++ mlops-project/pipeline.py
@@ -41,8 +41,11 @@ class MLPipeline:
             project_config_path.parent, project_config_path.stem
         )
         self.config.load()
+        logger.debug(f"The configurations loaded{self.config.items}")
+        # Build the pipeline by topologically sorting the task DAG
+        sorted_tasks = self.topological_sort(self.config.items.tasks)
         self.tasks = []
-        for task in self.tasks:
+        for task in sorted_tasks:
             try:
                 func = getattr(self, task)
             except AttributeError:
@@ -52,6 +55,15 @@ class MLPipeline:
                 self.tasks.append(func)
             else:
                 raise Exception(f"'{func}' is not a pipeline task.")
+        # create a Unix timestamp for the current run
+        self.timestamp = int(time.time())
+        logger.debug(self.timestamp)
+        logger.debug(self.config)
+        # create a directory to store training artifacts
+        self.artifact_dir = (
+            f"./artifacts/{self.config.project}/{self.timestamp}"
+        )
+        pathlib.Path(self.artifact_dir).mkdir(parents=True)
 
     @pipeline_task
     def load_data(self) -> None:
@@ -63,6 +75,21 @@ class MLPipeline:
         # ...
         pass
 
+    @pipeline_task
+    def feature_engineer_data(self) -> None:
+        pass
+
+    @pipeline_task
+    def train_model(self) -> None:
+        pass
+
+    @pipeline_task
+    def evaluate_model(self) -> None:
+        pass
+
+    @pipeline_task
+    def create_report(self) -> None:
+        pass
     def not_a_task(self, a, b) -> None:
         # ...
         pass
@@ -70,6 +97,41 @@ class MLPipeline:
     def run(self) -> None:
         for task in self.tasks:
             task()
+
+    def topological_sort(self, digraph: "omegaconf.DictConfig"):
+        """
+        Topological sorting for the pipeline tasks from the configuration yaml file
+        :param digraph:
+        :return:
+        """
+        # calculate indegree for all nodes
+        indegree = {node: 0 for node in digraph}
+        for node in digraph:
+            for adjacent_node in digraph[node].next:
+                if adjacent_node not in indegree:
+                    raise Exception(f"Node '{adjacent_node}' not defined")
+                indegree[adjacent_node] += 1
+
+        # get zero-indegree nodes
+        zero_indegree_nodes = [node for node in digraph if indegree[node] == 0]
+
+        # sort
+        sorted_nodes = []
+        while len(zero_indegree_nodes) > 0:
+            # add a zero-indegree node to the sorted array
+            node = zero_indegree_nodes.pop()
+            sorted_nodes.append(node)
+
+            # decrement the indegree of all adjacent nodes
+            for adjacent_node in digraph[node].next:
+                indegree[adjacent_node] -= 1
+                if indegree[adjacent_node] == 0:
+                    zero_indegree_nodes.append(adjacent_node)
+
+        if len(sorted_nodes) != len(digraph):
+            raise Exception("Tasks do not form a DAG")
+
+        return sorted_nodes
 if __name__ == "__main__":
     #Create the parser object, initializing it with the description of the program
     parser = argparse.ArgumentParser(
@@ -90,7 +152,7 @@ if __name__ == "__main__":
     args = parser.parse_args()
     logger = utils.Logger("ml-pipeline", debug=args.debug).get()
     logger.debug(args.config)
-
+    #Create the pipeline and run it
     try:
         pipeline = MLPipeline(args.config, logger)
         pipeline.run()
@@ -98,4 +160,4 @@ if __name__ == "__main__":
         logger.error(error)
         sys.exit(-1)
 
-    logger.info("Pipeline run complete")
+    logger.info(f"Pipeline run complete. See all artifacts in {pipeline.artifact_dir}")
